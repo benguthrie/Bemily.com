fd9f9315acd672fab572c631a76a8292d9675319
Ben Guthrie <guthrie.ben@gmail.com>
2018-02-27 12:51:21 -0600

    Added recipe to index
    Things to show Alexa

--- index.php
+++ index.php
@@ -2,6 +2,19 @@
 <?php
 echo "test";
 ?>
+<h1>Recipe for Baking Cookies</h1>
+
+<h2>Prep - Tools Required</h2>
+
+<h2>Ingredients</h2>
+
+<script type="text/javascript">
+	
+	
+</script>
+<h2>Baking Instructions</h2>
+
+
 
 <p>
 	These are some words I added.
